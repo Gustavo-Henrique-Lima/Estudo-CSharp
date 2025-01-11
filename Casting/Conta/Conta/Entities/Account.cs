3c9618e7f1118fce85d5fedca275a5bfd5ee4133
Gustavo Henrique <gustavohgustavo@hotmail.com>
2025-01-11 15:45:59 -0300

    Upcasting e downcasting.

---
+++ Casting/Conta/Conta/Entities/Account.cs
@@ -0,0 +1,32 @@
+﻿namespace Conta.Entities
+{
+    class Account
+    {
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        public Account() { }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+    }
+}
