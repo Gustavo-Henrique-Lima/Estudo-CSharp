0c7b9103ae86172a361f8de434ce2c3304f8c066
Gustavo Henrique <gustavohgustavo@hotmail.com>
2025-01-11 15:56:56 -0300

    Sobreposição.

---
+++ Sobreposicao/Conta/Conta/Entities/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿namespace Conta.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(double interestRate)
+        {
+            InterestRate = interestRate;
+        }
+
+        public SavingsAccount(int number, string holder, double balance,
+            double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void WithDraw(double amount)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+            }
+        }
+    }
+}
---
+++ Sobreposicao/Conta/Conta/Program.cs
@@ -0,0 +1,13 @@
+﻿using Conta.Entities;
+
+Account acc1 = new(1001, "Alex", 500.00);
+Account acc2 = new SavingsAccount(1002, "Ana", 500.00, 0.01);
+Account acc3 = new BusinessAccount(1003, "Bob", 500.00, 200.0);
+
+acc1.WithDraw(10.0);
+acc2.WithDraw(10.0);
+acc3.WithDraw(10.0);
+
+Console.WriteLine(acc1.Balance);
+Console.WriteLine(acc2.Balance);
+Console.WriteLine(acc3.Balance);
