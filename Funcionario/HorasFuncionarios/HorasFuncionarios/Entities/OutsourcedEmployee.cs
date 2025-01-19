426d9a4ea02cfcaf27d531c5eaf99163f73e2fe8
Gustavo Henrique <gustavohgustavo@hotmail.com>
2025-01-19 10:34:04 -0300

    Exercicio de fixação herança.

---
+++ Funcionario/HorasFuncionarios/HorasFuncionarios/Entities/OutsourcedEmployee.cs
@@ -0,0 +1,21 @@
+﻿namespace HorasFuncionarios.Entities
+{
+    class OutsourcedEmployee : Employee
+    {
+        public double AdditionalCharge { get; set; }
+
+        public OutsourcedEmployee() { }
+
+        public OutsourcedEmployee(string name, int hours, double valuePerHour,
+            double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        public override double Payment()
+        {
+           double additional = AdditionalCharge * 1.1;
+           return base.Payment() + additional;
+        }
+    }
+}
---
+++ Funcionario/HorasFuncionarios/HorasFuncionarios/Program.cs
@@ -0,0 +1,37 @@
+﻿using HorasFuncionarios.Entities;
+using System.Globalization;
+
+Console.Write("Enter the number of employees: ");
+int qtd = int.Parse(Console.ReadLine());
+List<Employee> employees = new List<Employee>();
+for (int i = 0; i < qtd; i++)
+{
+    Console.WriteLine($"Empolyee #{i + 1} data");
+    Console.Write("Outsourced (y/n)? ");
+    char outSorced = char.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Hours: ");
+    int hours = int.Parse(Console.ReadLine());
+    Console.Write("Value per hour: ");
+    double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    if(outSorced == 'n')
+    {
+        Employee em = new(name, hours, value);
+        employees.Add(em);
+    }
+    else
+    {
+        Console.Write("Additional charge: ");
+        double additional = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        OutsourcedEmployee e = new(name, hours, value, additional);
+        employees.Add(e);
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Payments: ");
+foreach (Employee employee in employees)
+{
+    double salary = employee.Payment();
+    Console.WriteLine($"{employee.Name} - $ {salary.ToString("F2", CultureInfo.InvariantCulture)}");
+}
